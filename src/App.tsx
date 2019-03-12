796e4861f107baf0dbd4fd886a37225ddbef6213
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-03-12 23:15:37 +0900

    feat: layout using bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,19 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import { Body, Chrome, Content, Footer, FooterItem as ChromeFooterItem, Header, HeaderItemWrapper } from "@zendeskgarden/react-chrome";
-import { ThemeProvider } from "@zendeskgarden/react-theming";
 
-import '@zendeskgarden/react-chrome/dist/styles.css';
 
-const FooterItem = styled(ChromeFooterItem)`
-  color: #6c757d;
-  font-size: 80%;
+const Root = styled.div`
+  padding: 10px 0;
 `;
 
 const App = () => (
-  <ThemeProvider>
-    <Chrome>
-      <Body hasFooter>
-        <Header standalone>
-          <HeaderItemWrapper logo>
-            P
-          </HeaderItemWrapper>
-          <HeaderItemWrapper maxX>
-            <span>Pixiv Query Builder</span>
-          </HeaderItemWrapper>
-        </Header>
-        <Content />
-        <Footer>
-          <FooterItem>
-            Licensed under the <a href="https://github.com/mika-sandbox/react-practice/blob/master/LICENSE" target="_blank" rel="noopener noreferrer">MIT License</a>
-          </FooterItem>
-          <FooterItem>
-            &copy; {new Date().getFullYear()} mika-f
-          </FooterItem>
-        </Footer>
-      </Body>
-    </Chrome>
-  </ThemeProvider>
+  <React.StrictMode>
+    <Root className="container">
+      <h2>pixiv query builder</h2>
+      <p>pixiv の画像検索を良い感じにすることが出来ます。</p>
+
+    </Root>
+  </React.StrictMode>
 );
 
 export default App;
